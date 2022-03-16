d99e18f71ae8037ec6930a1e0307f586cdf7f1c5
smartcode108 <smartbrainnetwork@gmail.com>
2022-03-15 20:41:36 -0700

    ggg

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,6 @@
-#include"main.h"
-
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
 /**
 * main - Entry point
 *
@@ -7,16 +8,6 @@
 */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
-	
+	printf("_putchar\n");
 	return (0);
 }
