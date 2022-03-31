c242387ac4e9b8281c47f364eb57ab255faf88f6
smartcode108 <smartbrainnetwork@gmail.com>
2022-03-31 23:01:57 +0100

    hhh

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,11 +1,13 @@
+#include "holberton.h"
+
 /**
 * set_string - function that sets the value of a pointer to a char
-* @s:  pointer to the adderss of string s
-* @to: string to set value to
+* @s: pointer to the address of strings
+* @to: strings to set value to
 *
-* Return: nothing
- */
-void set_string(char **s, char *to)
+Return: nothing
+*/
+void set_string(char**s,char*to)
 {
-	*s = to;
+*s= to;
 }
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,2 @@
+C-Recursion. We going deeper
+
